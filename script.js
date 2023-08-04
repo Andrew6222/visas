5416939ceee23ec5fdde0692199f0246545646f1
Andrew <andrey.beschetnov@bk.ru>
2023-08-04 23:08:28 +0400

    commit6

--- script.js
+++ script.js
@@ -76,3 +76,20 @@ modalOverlay.addEventListener("click", (e) => {
     });
   }
 });
+
+// nav dropdown
+function openDropdown() {
+  document.getElementById("dropdown").classList.toggle("drpdownshow");
+}
+window.onclick = function (event) {
+  if (!event.target.matches(".dropdown-nav-link")) {
+    var dropdowns = document.getElementsByClassName("nav-sublist");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("drpdownshow")) {
+        openDropdown.classList.remove("drpdownshow");
+      }
+    }
+  }
+};
