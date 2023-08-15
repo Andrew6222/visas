458f0bc6fcfcd30f885d495f58e33e6dbacc72b7
Andrew <andrey.beschetnov@bk.ru>
2023-08-15 17:13:06 +0400

    commit25

--- script.js
+++ script.js
@@ -34,16 +34,25 @@ updateSlider();
 setInterval(showPreviousSlide, 3000);
 // hero slider
 
-let hero1 = document.getElementById("hero1");
-let hero2 = document.getElementById("hero2");
+let elems = [
+  {id: document.getElementById("hero1"), status: 'open'},
+  {id: document.getElementById("hero2"), status: 'close'}
+];
+
 displayFunc = () => {
-  hero1.classList.toggle("displaynone");
-  hero2.classList.toggle("displaynone");
-  // hero1.classList.toggle("hiddenblock");
-  // hero2.classList.toggle("hiddenblock");
-  // hero1.classList.toggle("showblock");
-  // hero2.classList.toggle("showblock");
+  elems.forEach((elem) => {
+    if (elem.status === 'open') {
+      elem.status = 'close';
+      elem.id.classList.add("displaynone");
+      elem.id.classList.remove("showblock");
+    } else {
+      elem.status = 'open';
+      elem.id.classList.remove("displaynone");
+      elem.id.classList.add("showblock");
+    }
+  });
 };
+
 setInterval(() => {
   displayFunc();
 }, 10000);
@@ -80,21 +89,22 @@ modalOverlay.addEventListener("click", (e) => {
 // nav dropdown
 function openDropdown() {
   document.getElementById("dropdown").classList.toggle("drpdownshow");
-}
-window.onclick = function (event) {
-  if (!event.target.matches(".dropdown-nav-link")) {
-    var dropdowns = document.getElementsByClassName("nav-sublist");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains("drpdownshow")) {
-        openDropdown.classList.remove("drpdownshow");
+
+  window.onclick = function (event) {
+    if (!event.target.matches(".dropdown-nav-link")) {
+      var dropdowns = document.getElementById("dropdown")
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains("drpdownshow")) {
+          openDropdown.classList.remove("drpdownshow");
+        }
       }
     }
-  }
-};
+  };
+}
 
 window.addEventListener("load", () => {
   document.getElementById("circle").classList.add("circle-append");
-  // document.getElementById("hero1").classList.add("hero1show");
+  document.getElementById("hero1").classList.add("hero1show");
 });
